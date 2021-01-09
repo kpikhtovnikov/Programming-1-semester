6081a922d8d643939dca7909f699eb33bfad2eb0
Kirill <pihtovnikovkirill@gmail.com>
2021-01-09 17:56:24 +0300

    уравнения

---
+++ уравнения/Source1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int x, i=0,k = 0;
+	char a[],c;
+	scanf("s", c);
+	while ( b!= 13)
+	{
+		b = getch(c);
+		a[i] = c;
+		i++;
+		scanf("s", c);
+	}
+	for (j = 0; j << i, j++)
+	{
+		if(a[j])
+	}
+
+}
